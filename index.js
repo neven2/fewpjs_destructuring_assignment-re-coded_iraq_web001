b303f1a2c29bc3e0b60b9d7f0ae79eea6342e061
Neven Bedr <neven2225@gmail.com>
2020-05-21 19:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ console.log(moo, neigh, baa, oink, cluck);
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
 const [Bessie, , Dolly, Babe, Little] = farmAnimals
-console.log(Bessie, Dolly, Babe, Little)
+console.log(Bessie, Dolly, Babe, Little);
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 const [black_and_white, , black, pink, ,] = farmAnimals
 console.log(black_and_white, black, pink)
